c5151baf07a5253c355def3c231127ca2eb73f34
Arron <GreenArronic@gmail.com>
2024-02-24 20:52:43 +0330

    feat: change logo slideup animation with a bounce

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -11,7 +11,7 @@ export default function Start({ onStart }) {
 
   if (fadeAway) {
     // dangerous, but we don't render Start after this
-    setTimeout(() => onStart(true), 300);
+    setTimeout(() => onStart(true), 500);
   }
 
   return (
